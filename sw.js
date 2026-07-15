71ab6ff9935674afff7a940240ffee5bd19328cd
Al-Rafidain <mjtly63@gmail.com>
2026-07-15 21:18:36 +0300

    Refactor service worker to use cache and fetch events

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-const FILES = ['/rafidain-sales-app/','/rafidain-sales-app/index.html','/rafidain-sales-app/manifest.json','/rafidain-sales-app/icon-192.png','/rafidain-sales-app/icon-512.png'];
+    const CACHE_NAME = 'rafidain-v1';
+    const FILES = [
+      '/rafidain-sales-app/',
+      '/rafidain-sales-app/index.html',
+      '/rafidain-sales-app/manifest.json',
+      '/rafidain-sales-app/icon-192.png',
+      '/rafidain-sales-app/icon-512.png'
+    ];
+    self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES))));
+    self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
