2a3410e86f1849c6a256cabba4da518ec55668cb
Al-Rafidain <mjtly63@gmail.com>
2026-07-26 01:02:28 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = "rafidain-v38";
-self.addEventListener('install', e=>{self.skipWaiting();});
-self.addEventListener('activate', e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!=CACHE&&caches.delete(k)))))});
-self.addEventListener('fetch', e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
